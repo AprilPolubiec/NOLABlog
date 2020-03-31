4d462741316c56f44df2c1b599708da43e56afe7
April Polubiec <aprilpolubiec@gmail.com>
2020-03-31 18:13:55 -0500

    shrink hero on button click

--- js/script.js
+++ js/script.js
@@ -1,45 +1,51 @@
-//Fill category list
+$(document).ready(function() {
+  //Fill category and visit length lists
+  var categories = {
+    food: 'Foodie',
+    music: 'Music Fan',
+    celebrity: 'Celebrity Enthusiast',
+    party: 'Party Animal',
+    fashion: 'Fashion Guru',
+    history: 'History Buff',
+    art: 'Art Aficionado',
+    nature: 'Nature Lover'
+  }
+  //Instagrammer
 
-var categories = {
-  food: 'Foodie',
-  music: 'Music Fan',
-  celebrity: 'Celebrity Enthusiast',
-  party: 'Party Animal',
-  fashion: 'Fashion Guru',
-  history: 'History Buff',
-  art: 'Art Aficionado',
-  nature: 'Nature Lover'
-}
+  var lengths = {
+    threeDays: '3 days',
+    fiveDays: '5 days',
+    oneWeek: '1 week',
+    twoWeeks: '2 weeks'
+  }
 
-var lengths = {
-  threeDays: '3 days',
-  fiveDays: '5 days',
-  oneWeek: '1 week',
-  twoWeeks: '2 weeks'
-}
+  Object.entries(categories).forEach(function([key, value]) {
+    var optionValue = key
+    var text = value
+    var el = document.createElement('option')
+    el.textContent = text
+    el.value = optionValue
+    $('#categories').append(el)
+  })
 
-var categoryListEl = document.getElementById('categories')
-var lengthsEl = document.getElementById('lengths')
+  Object.entries(lengths).forEach(function([key, value]) {
+    var optionValue = key
+    var text = value
+    var el = document.createElement('option')
+    el.textContent = text
+    el.value = optionValue
+    $('#lengths').append(el)
+  })
 
-Object.entries(categories).forEach(function([key, value]) {
-  var optionValue = key
-  var text = value
-  var el = document.createElement('option')
-  el.textContent = text
-  el.value = optionValue
-  categoryListEl.appendChild(el)
+  //On button click
+  $('#shiny-button').click(function() {
+    var category = $('#categories')
+      .children('option:selected')
+      .val()
+    var length = $('#lengths')
+      .children('option:selected')
+      .val()
+    $('#hero').addClass('small')
+    $("#post-container").css("display", "flex")
+  })
 })
-
-Object.entries(lengths).forEach(function([key, value]) {
-  var optionValue = key
-  var text = value
-  var el = document.createElement('option')
-  el.textContent = text
-  el.value = optionValue
-  lengthsEl.appendChild(el)
-})
-
-function displayCategories() {
-  var categoryEl = document.getElementsByClassName('category-container')[0]
-  categoryEl.classList.add('open')
-}
